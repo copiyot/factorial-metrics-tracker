2927375e34812fa36d3afdae8c4abe5ef0c7d765
Castro Opiyo <copiyot@Castros-MacBook-Pro.local>
2022-04-26 21:28:50 +0300

    feat: add env file

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,13 +6,15 @@ import express from "express";
 import {ApolloServer} from "apollo-server-express";
 import {buildSchema} from "type-graphql";
 import { TokenResolver } from "./resolvers/token";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);
     await orm.getMigrator().up();
     
     const app = express();
-    // app.use(cors({origin: "http://localhost:3000", credentials: true}));
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
@@ -25,7 +27,7 @@ const main = async () => {
     await apolloServer.start();
     apolloServer.applyMiddleware({app});
 
-    app.listen(4000, () => console.log("Listening on port 4000...."));
+    app.listen(process.env.PORT, () => console.log("Listening on port 4000...."));
 };
 
 main().catch(err => console.error(err));
--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -2,6 +2,9 @@ import { __prod__ } from "./constants";
 import { Token } from "./entities/Token";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 export default {
     migrations: {
@@ -9,9 +12,10 @@ export default {
         glob: '!(*.d).{js,ts}',
     },
     entities:[Token],
-    dbName: 'factorial',
+    dbName: process.env.DB_NAME,
     type: 'postgresql',
-    user: 'copiyot',
+    user: process.env.USER,
+    password: process.env.PASSWORD,
     debug: !__prod__,
     allowGlobalContext: true
 } as Parameters<typeof MikroORM.init>[0];
